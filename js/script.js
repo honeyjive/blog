fd300f2834785af06804d2bad519aa020c8fa480
Honey Son <honeyson@honeys-mbp.gateway.2wire.net>
2014-09-25 22:23:14 -0500

    adding borders

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
-    console.log( "I loaded in jQuery and my script file!" );
-    $('body').addClass('js-test');
-});
+// $( document ).ready(function() {
+//     console.log( "I loaded in jQuery and my script file!" );
+//     $('body').addClass('js-test');
+// });
